b0fea9e1c1b22d70b23f39d913509b7f0fd1eb19
George_Chakama <gtchakama@gmail.com>
2021-11-19 01:25:56 +0200

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div>
-      <h1 className="text-indigo-600 text-7xl">Rate Exchange</h1>
+      <h1 className="text-indigo-600 text-7xl mb-9">Rate Exchange</h1>
     </div>
   );
 }
